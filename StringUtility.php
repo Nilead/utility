5221df02a6f40e5d5835be5973a2d12dc4202c2e
Vu Nguyen <yellow1912@gmail.com>
2021-04-01 10:57:42 +0700

    Add camelize function

--- StringUtility.php
+++ StringUtility.php
@@ -455,4 +455,28 @@ class StringUtility
 
         return $plural;
     }
+
+    /**
+     * Camelizes a string.
+     *
+     * @param string $id A string to camelize
+     *
+     * @return string The camelized string
+     */
+    public static function camelize($id)
+    {
+        return strtr(ucwords(strtr($id, ['_' => ' ', '.' => '_ ', '\\' => '_ '])), [' ' => '']);
+    }
+
+    /**
+     * A string to underscore.
+     *
+     * @param string $id The string to underscore
+     *
+     * @return string The underscored string
+     */
+    public static function underscore($id)
+    {
+        return strtolower(preg_replace(['/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'], ['\\1_\\2', '\\1_\\2'], str_replace('_', '.', $id)));
+    }
 }
